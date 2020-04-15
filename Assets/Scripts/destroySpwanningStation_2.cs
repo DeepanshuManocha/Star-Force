6c89659c39e1ce150127d5e9a4e6b530e547a7cb
DeepanshuManocha <dmanocha1509@gmail.com>
2020-04-16 03:21:04 +0530

    done

---
+++ Assets/Scripts/destroySpwanningStation_2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroySpwanningStation_2 : MonoBehaviour
+{
+    public float health = 10f;
+    public GameObject explosion;
+    public GameObject explosion2;
+    public void Takedamage()
+    {
+        health -= 1f;
+        enemyHealth2.enemyhealth_2 = health;
+
+        // damage-=1f;
+        //enemyHealth.health=damage;
+        //damage=enemyHealth.health;
+        //damage-=1f;
+
+        if (enemyHealth.enemyhealth == 0)
+        {
+            int a = Random.Range(0, 1);
+            if (a == 0)
+            {
+                explosion.SetActive(true);
+
+            }
+            if (a == 1)
+            {
+                explosion2.SetActive(true);
+            }
+            InvokeRepeating("die", 2, 0);
+        }
+    }
+
+    void die()
+    {
+        Destroy(gameObject);
+        Debug.Log("destroy");
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Commandos
+Commandos is my final Augmented Reality project made in ARcore and player will be able to play anywhere just by scanning the surface with the help of any Console controller.
